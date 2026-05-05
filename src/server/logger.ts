9523cbac60011c25b70f8be4fcf2331a69042b79
sneak-it <16104273+sneak-it@users.noreply.github.com>
2026-05-04 22:32:57 -0400

    remove hostname and pid from logging

--- src/server/logger.ts
+++ src/server/logger.ts
@@ -3,6 +3,7 @@ import pino from 'pino';
 export const logger = pino({
   level: process.env.LOG_LEVEL ?? 'info',
   timestamp: pino.stdTimeFunctions.isoTime,
+  base: undefined,
   transport:
     process.env.NODE_ENV === 'production'
       ? undefined
